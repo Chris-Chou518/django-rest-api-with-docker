13804a297dff9effd576108407b1d8505263b41b
Chris <ttlyes496@gmail.com>
2024-09-13 15:32:16 +0800

    Configure celery, redis, flower

--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -11,3 +11,10 @@ SECRET_KEY = env(
 DEBUG = True
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"] #Django 中用來定義信任的來源域名的設置，專門用來允許特定來源的網站進行跨站請求偽造（CSRF, Cross-Site Request Forgery）保護下的安全請求。
+
+EMAIL_BACKEND="djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST=env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT=env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL="ttlyes496@gmail.com"
+DOMAIN=env("DOMAIN")
+SITE_NAME= "Authors Haven"
